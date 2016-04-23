7ae6db31e153df8fadfc700a9ba1ec6838e2f641
huangyibiao <huangyibiao@huangyibiaodeMacBook-Pro.local>
2016-04-24 01:20:57 +0800

    update

--- JavaScriptAndObjectiveC/JavaScriptAndObjectiveC/ViewController.m
+++ JavaScriptAndObjectiveC/JavaScriptAndObjectiveC/ViewController.m
@@ -22,20 +22,20 @@
   
   [self.view addSubview:self.webView];
  
-  // 一个JSContext对象，就类似于Js中的window，只需要创建一次即可。
-  self.jsContext = [[JSContext alloc] init];
-
-  // jscontext可以直接执行JS代码。
-  [self.jsContext evaluateScript:@"var num = 10"];
-  [self.jsContext evaluateScript:@"var squareFunc = function(value) { return value * 2 }"];
-  // 计算正方形的面积
-  JSValue *square = [self.jsContext evaluateScript:@"squareFunc(num)"];
-  
-  // 也可以通过下标的方式获取到方法
-  JSValue *squareFunc = self.jsContext[@"squareFunc"];
-  JSValue *value = [squareFunc callWithArguments:@[@"20"]];
-  NSLog(@"%@", square.toNumber);
-  NSLog(@"%@", value.toNumber);
+//  // 一个JSContext对象，就类似于Js中的window，只需要创建一次即可。
+//  self.jsContext = [[JSContext alloc] init];
+//
+//  // jscontext可以直接执行JS代码。
+//  [self.jsContext evaluateScript:@"var num = 10"];
+//  [self.jsContext evaluateScript:@"var squareFunc = function(value) { return value * 2 }"];
+//  // 计算正方形的面积
+//  JSValue *square = [self.jsContext evaluateScript:@"squareFunc(num)"];
+//  
+//  // 也可以通过下标的方式获取到方法
+//  JSValue *squareFunc = self.jsContext[@"squareFunc"];
+//  JSValue *value = [squareFunc callWithArguments:@[@"20"]];
+//  NSLog(@"%@", square.toNumber);
+//  NSLog(@"%@", value.toNumber);
 }
 
 - (UIWebView *)webView {
@@ -53,17 +53,21 @@
 
 #pragma mark - UIWebViewDelegate
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
-  self.jsContext = [webView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
-  // 通过模型调用方法，这种方式更好些。
-   HYBJsObjCModel *model  = [[HYBJsObjCModel alloc] init];
-  self.jsContext[@"OCModel"] = model;
-  model.jsContext = self.jsContext;
-  model.webView = self.webView;
+    self.jsContext = [webView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    // 通过模型调用方法，这种方式更好些。
+    HYBJsObjCModel *model  = [[HYBJsObjCModel alloc] init];
+    self.jsContext[@"OCModel"] = model;
+    model.jsContext = self.jsContext;
+    model.webView = self.webView;
+    
+    self.jsContext.exceptionHandler = ^(JSContext *context, JSValue *exceptionValue) {
+      context.exception = exceptionValue;
+      NSLog(@"异常信息：%@", exceptionValue);
+    };
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
   
-  self.jsContext.exceptionHandler = ^(JSContext *context, JSValue *exceptionValue) {
-    context.exception = exceptionValue;
-    NSLog(@"异常信息：%@", exceptionValue);
-  };
 }
 
 @end
